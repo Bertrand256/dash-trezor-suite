85d4fd362f21aee58e7b5c8c5e5bbaedb2a02a62
Vojtěch Tranta <vojta.tranta@gmail.com>
2026-02-02 10:48:29 +0100

    feat: go to fw installation right from the device select when fw is required

--- packages/suite/src/views/suite/SwitchDevice/NeedsAttentionBanner.tsx
+++ packages/suite/src/views/suite/SwitchDevice/NeedsAttentionBanner.tsx
@@ -101,6 +101,11 @@ export const NeedsAttentionBanner = ({
 
     const createOnIssueClickHandler = (): (() => void) | null => {
         switch (deviceStatus) {
+            case 'firmware-required':
+                return () => {
+                    dispatch(selectDeviceThunk({ device }));
+                    dispatch(goto('firmware-index'));
+                };
             // If onboarding is pending, then it should pass through Manual Device Check.
             case 'initialize': // Wiped device with firmware present.
             case 'bootloader': // Fresh or factory-reset device? Can also be initalized device manually put into BL,
@@ -111,7 +116,6 @@ export const NeedsAttentionBanner = ({
                 };
 
             case 'seedless':
-            case 'firmware-required':
             case 'firmware-corrupted':
             case 'unavailable':
             case 'unreadable':
