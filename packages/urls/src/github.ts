3bee27a77c8db79da8f92934baed94dd09606884
Bertrand256 <blogin@protonmail.com>
2026-02-06 15:32:59 +0100

    Replace GitHub links with Dash Trezor Suite equivalents and update release workflow to handle release notes dynamically.

--- packages/urls/src/github.ts
+++ packages/urls/src/github.ts
@@ -1,8 +1,8 @@
 import { Url } from './types';
 
 const GITHUB_REPO_INFO = {
-    owner: 'trezor',
-    repo: 'trezor-suite',
+    owner: 'Bertrand256',
+    repo: 'dash-trezor-suite',
 } as const;
 
 export const GITHUB_REPO_URL: Url =
@@ -10,7 +10,7 @@ export const GITHUB_REPO_URL: Url =
 export const GITHUB_API_REPO_URL: Url =
     `https://api.github.com/repos/${GITHUB_REPO_INFO.owner}/${GITHUB_REPO_INFO.repo}` as const;
 
-export const GITHUB_ROADMAP_URL: Url = 'https://github.com/orgs/trezor/projects/28?fullscreen=true';
+export const GITHUB_ROADMAP_URL: Url = 'https://github.com/Bertrand256/dash-trezor-suite/issues';
 export const GITHUB_FW_COMMIT_URL: Url = 'https://github.com/trezor/trezor-firmware/commit/';
 export const GITHUB_FW_BINARIES_URL: Url =
     'https://github.com/trezor/webwallet-data/tree/master/firmware';
