6892e23a1f99642c915892ec0d48286db4f0db2a
Bertrand256 <blogin@protonmail.com>
2026-02-07 00:07:57 +0100

    Change icons and UI color palette to use blue tones.

--- packages/suite/src/views/dashboard/index.tsx
+++ packages/suite/src/views/dashboard/index.tsx
@@ -7,7 +7,6 @@ import { PageHeader } from 'src/components/suite/layouts/SuiteLayout';
 import { AssetsView } from './AssetsView/AssetsView';
 import { PortfolioCard } from './PortfolioCard/PortfolioCard';
 import { PromoBanner } from './PromoBanner';
-import { T3T1PromoBanner } from './T3T1PromoBanner/T3T1PromoBanner';
 import { StakeEthCard } from './StakeEthCard/StakeEthCard';
 import { DashboardPassphraseBanner } from './DashboardPassphraseBanner';
 
@@ -18,7 +17,6 @@ export const Dashboard = () => {
         <Column gap={spacings.xxxxl} data-testid="@dashboard/index">
             <DashboardPassphraseBanner />
             <PortfolioCard />
-            <T3T1PromoBanner />
             <AssetsView />
             <StakeEthCard />
             <PromoBanner />
